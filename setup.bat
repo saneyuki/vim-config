b9a0529e17d905d20777bdd79234989fbf4cb463
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-01-09 18:01:56 +0900

    Create ~/.vim link on Windows

--- setup.bat
+++ setup.bat
@@ -8,9 +8,10 @@ rem remove old symbolic links
 rm %USERPROFILE%\.vimrc
 rm %USERPROFILE%\.gvimrc
 rd %USERPROFILE%\vimfiles
+rd %USERPROFILE%\.vim
 
 rem create new symbolic links
 mklink %USERPROFILE%\.vimrc  %CD%\vimrc
 mklink %USERPROFILE%\.gvimrc  %CD%\gvimrc
 mklink /D %USERPROFILE%\vimfiles  %CD%\vimfiles
-
+mklink /D %USERPROFILE%\.vim  %CD%\vimfiles
